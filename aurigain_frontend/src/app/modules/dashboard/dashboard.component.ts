8dd8f0c752b70877cefcdab8f1b5f7782b46d72c
Faizan Ahmad <sunny.faizan@gmail.com>
2022-02-09 14:05:17 +0530

    sidebar overlap in mob & dropdown open left - fixed

--- aurigain_frontend/src/app/modules/dashboard/dashboard.component.ts
+++ aurigain_frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SideNavBarService } from './side-nav.service';
+import { HostListener } from "@angular/core";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,31 @@ import { SideNavBarService } from './side-nav.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
   toggle:boolean=false;
+  screenHeight: number;
+  screenWidth: number;
+  marginWidth: any;
+
   constructor(
     public sidenavservice: SideNavBarService
-  ) { }
+  ) {
+    this.getScreenSize();
+   }
+
+   @HostListener('window:resize', ['$event'])
+   getScreenSize(event?) {
+         this.screenHeight = window.innerHeight;
+         this.screenWidth = window.innerWidth;
+         console.log(this.screenHeight, this.screenWidth);
+
+         if(this.screenWidth > 992){
+           this.marginWidth = 250 + 'px';
+         }
+         else{
+           this.marginWidth = 0 + 'px';
+         }
+   }
 
   toggled(){
     this.toggle = !this.toggle
